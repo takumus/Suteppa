290e5034676718aa5da837a0b1ee40cdbe4e7705
takumus <me@takumus.com>
2016-09-14 23:21:14 +0900

    サンプルコード更新

--- src/main.cpp
+++ src/main.cpp
@@ -1,58 +1,99 @@
 #include <Arduino.h>
 #include "Suteppa.h"
-Suteppa s;
 const int IN1 = 9;
 const int IN2 = 10;
 const int IN3 = 11;
 const int IN4 = 12;
-int d_hs[9] = {
-	B01000, 
-	B01100,
-	B00100,
-	B00110,
-	B00010,
-	B00011,
-	B00001,
-	B01001,
-	B00000
+const int MAX = 495;
+int d_hs[8] = {
+	B1000, 
+	B1100,
+	B0100,
+	B0110,
+	B0010,
+	B0011,
+	B0001,
+	B1001
 };
-int i = 0;
+Suteppa s;
+
 void step(int d);
+int findBegin(int step);
 void setup()
 {
-	Serial.begin(9600);
 	pinMode(IN1, OUTPUT);
 	pinMode(IN2, OUTPUT);
 	pinMode(IN3, OUTPUT);
 	pinMode(IN4, OUTPUT);
 	pinMode(2, INPUT_PULLUP);
-	pinMode(3, INPUT_PULLUP);
-	s.init(1024*4, 200, 6000, 850, true, step);
+	//一回転のステップ数, 加減速に使うステップ数, 開始速度, 最大速度, ステップ用関数
+	s.init(500, 50, 2000, 1000, step);
 }
 void loop()
 {
+	//----------------------------------------------//
+	//キャリブレーション
+	//----------------------------------------------//
+	//スムーズオフ
+	s.setSmooth(false);
+	//速度を2000マイクロ秒に
+	s.setMax(2000);
+	//先頭まで戻る
+	s.rotate(Suteppa::RELATIVE, -9999, findBegin);
+	//先頭を見つけたら、もう一度繰り返す
+	delay(100);
+	s.rotate(Suteppa::RELATIVE, 20);
+	delay(100);
+	s.rotate(Suteppa::RELATIVE, -20, findBegin);
+	delay(1000);
+
+	//----------------------------------------------//
+	//耐久テストセットアップ
+	//----------------------------------------------//
+
+	//スムーズ
+	s.setSmooth(true);
+	//初速度
+	s.setBegin(2000);
+	//最大速度
+	s.setMax(950);
+	//アクションごとの待ち時間
+	int wait = 0;
+
+	//----------------------------------------------//
+	//耐久テスト開始
+	//----------------------------------------------//
 	while(true){
+		s.rotate(Suteppa::ABSOLUTE, MAX/2 + 100);
+		s.rotate(Suteppa::RELATIVE, -200);
+		s.rotate(Suteppa::RELATIVE, 200);
+		s.rotate(Suteppa::RELATIVE, -200);
+		s.rotate(Suteppa::RELATIVE, 200);
 		delay(1000);
-		s.rotateR(1024*3);
-		s.rotateR(-1024*3);
-		s.rotateR(1024);
-		s.rotateR(-1024);
-		s.rotateR(1024);
-		s.rotateR(-1024*3);
-		s.rotateR(1024*2);
-		s.rotateR(-1024*2);
-		s.rotateR(1024*3);
-		s.rotateR(-1024*2);
-		s.rotateA(0, true);
+		s.rotate(Suteppa::RELATIVE, -200);
 		delay(1000);
+		s.rotate(Suteppa::ABSOLUTE, MAX);
+		s.rotate(Suteppa::RELATIVE, -MAX);
+		s.rotate(Suteppa::ABSOLUTE, MAX);
+		s.rotate(Suteppa::RELATIVE, -MAX);
 	}
 }
+//先頭を見つける関数。
+int findBegin(int step)
+{
+	if(digitalRead(2) == 0){
+		s.setHome();
+		return 1;
+	}
+	return 0;
+}
+//ステッピングモーターを回す関数
 void step(int d)
 {
+	static int i;
 	i += d;
 	if(i > 7) i = 0;
 	if(i < 0) i = 7;
-	if(d == 0)i = 8;
 	byte b = d_hs[i];
 	digitalWrite(IN1, bitRead(b, 0));
 	digitalWrite(IN2, bitRead(b, 1));
